29005950addf1a9be9d81e463bbb056597560ecd
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-05-24 08:41:09 +0700

    Create : first init

---
+++ src/main/java/GameEngine.java
@@ -0,0 +1,33 @@
+
+
+public class GameEngine implements IGameEngine{
+    @Override
+    public IParty createPlayerParty(String partyName, ICharacter ICharacter1, ICharacter ICharacter2, ICharacter ICharacter3) {
+        return null;
+    }
+
+    @Override
+    public IParty createCPUParty(String partyName, ICharacter ICharacter1, ICharacter ICharacter2, ICharacter ICharacter3) {
+        return null;
+    }
+
+    @Override
+    public void saveGame() {
+
+    }
+
+    @Override
+    public void loadGame() {
+
+    }
+
+    @Override
+    public IParty nextTurn() {
+        return null;
+    }
+
+    @Override
+    public IParty winner() {
+        return null;
+    }
+}
---
+++ src/main/java/IParty.java
@@ -0,0 +1,10 @@
+
+
+public interface IParty {
+    public ICharacter nextPlayer();
+
+    public boolean hasNextPlayer();
+
+    // untuk mengetahui apakah semua character pada party sudah dikalahkan (HP <= 0)
+    public boolean isDefeated();
+}
