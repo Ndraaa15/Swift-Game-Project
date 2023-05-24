65ec4af2613230bd212fc77e3a5d76b9d74a421d
Gede Indra Adi Brata <112854205+Ndraaa15@users.noreply.github.com>
2023-05-24 08:47:10 +0700

    Delete src directory

--- src/IParty.java
+++
@@ -1,8 +0,0 @@
-public interface IParty {
-    public ICharacter nextPlayer();
-
-    public boolean hasNextPlayer();
-
-    // untuk mengetahui apakah semua character pada party sudah dikalahkan (HP <= 0)
-    public boolean isDefeated();
-}
