7f8a9ca03771a4ae87ce68cac0efcda0caa7c792
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-05-24 08:59:56 +0700

    Create : display game

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -47,6 +47,44 @@ public class Display {
         System.out.println("|====================================================================|");
     }
 
+    static void menuGame (){
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("|                               Menu                                |");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("|                           [a] New Game                            |");
+        System.out.println("|                           [b] Load Game                           |");
+        System.out.println("|                           [c] Exit Game                           |");
+        System.out.println("|                                                                   |");
+        System.out.println("|                             Swift Game                            |");
+        System.out.println("---------------------------------------------------------------------");
+    }
 
+    static void newGameMenu (){
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("|                              Options                              | ");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("|                           [a] Create Party                        |");
+        System.out.println("|                           [b] Create Hero                         |");
+        System.out.println("|                           [c] Back                                |");
+        System.out.println("|                                                                   |");
+        System.out.println("|                             Swift Game                            |");
+        System.out.println("---------------------------------------------------------------------");
+    }
+
+    static void readyGameMenu () {
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("|                            TEAM NAME                               |");
+        System.out.println("|====================================================================|");
+        System.out.println("|                      |                      |                      |");
+        System.out.println("|                      |                      |                      |");
+        System.out.println("|----------------------|----------------------|----------------------|");
+        System.out.println("|  HP  |  DEF  |  ATK  |  HP  |  DEF  |  ATK  |  HP  |  DEF  |  ATK  |");
+        System.out.println("|----------------------|----------------------|----------------------| ");
+        System.out.println("|      |       |       |      |       |       |      |       |       |");
+        System.out.println("|====================================================================|");
+        System.out.println("|                         Start play (Y/n) ?                         |");
+        System.out.println("----------------------------------------------------------------------");
+
+    }
 
 }
