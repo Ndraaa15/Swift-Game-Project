3f0bbed65bb6ccd3b96753d20ac152c415dad77c
Gede Indra Adi Brata <indrabrata599@gmail.com>
2023-06-08 21:17:47 +0700

    Adding name team

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,13 @@
+/*
+* Team :
+* -) Gede Indra Adi Brata (225150200111006)
+* -) LUCAS CHANDRA (225150200111009)
+* -) JAVIER AAHMES REANSYAH (225150201111001)
+* -) RAFIQI AHMAD HANAFIAH NASUTION (225150207111011)
+* -) GRATIA YUDIKA MORADO SILALAHI (225150207111015)
+*/
+
+
 public class App {
     public static void main(String[] args) {
         BaseGameHeroes heroes = new BaseGameHeroes();
